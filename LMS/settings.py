f1307cba449bc87a5bbafa691b96063314a424a3
Aradhya2002 <aradhyaram7@gmail.com>
2022-11-12 14:57:06 +0530

    as

--- LMS/settings.py
+++ LMS/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 ASGI_APPLICATION = 'LMS.asgi.application'
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, '/media/')
+
+STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
